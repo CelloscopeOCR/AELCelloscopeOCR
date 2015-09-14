b7875ae37c97ff9be2e7d61a87b059ad163a00e9
zamanAEL <assad@N5.ael.net>
2015-09-14 13:33:36 +0600

    Add TouchListener

--- Eclipse Ant Project/AELCelloscopeOCR/src/com/ael/celloscope/aelcelloscopeocr/BitmapEffectActivity.java
+++ Eclipse Ant Project/AELCelloscopeOCR/src/com/ael/celloscope/aelcelloscopeocr/BitmapEffectActivity.java
@@ -3,16 +3,16 @@ package com.ael.celloscope.aelcelloscopeocr;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
+import android.graphics.Matrix;
 import android.os.Bundle;
-
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-
 
 import com.ael.celloscope.aelcelloscopeocr.mediaeffects.VerticalSeekBar;
 
@@ -157,5 +157,69 @@ public class BitmapEffectActivity extends Activity implements
 		if (tempTargetBitmap != null) {
 			tempTargetBitmap.recycle();
 		}
+	}
+
+	private ImageView dialer;
+	private int dialerHeight, dialerWidth;
+	private static Matrix matrix;
+
+	private class MyOnTouchListener implements OnTouchListener {
+
+		private double startAngle;
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+
+			switch (event.getAction()) {
+
+			case MotionEvent.ACTION_DOWN:
+				startAngle = getAngle(event.getX(), event.getY());
+				break;
+
+			case MotionEvent.ACTION_MOVE:
+				double currentAngle = getAngle(event.getX(), event.getY());
+				rotateDialer((float) (startAngle - currentAngle));
+				startAngle = currentAngle;
+				break;
+
+			case MotionEvent.ACTION_UP:
+
+				break;
+			}
+
+			return true;
+		}
+	}
+
+	private double getAngle(double xTouch, double yTouch) {
+		double x = xTouch - (dialerWidth / 2d);
+		double y = dialerHeight - yTouch - (dialerHeight / 2d);
+
+		switch (getQuadrant(x, y)) {
+		case 1:
+			return Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+		case 2:
+			return 180 - Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+		case 3:
+			return 180 + (-1 * Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI);
+		case 4:
+			return 360 + Math.asin(y / Math.hypot(x, y)) * 180 / Math.PI;
+		default:
+			return 0;
+		}
+	}
+
+	private static int getQuadrant(double x, double y) {
+		if (x >= 0) {
+			return y >= 0 ? 1 : 4;
+		} else {
+			return y >= 0 ? 2 : 3;
+		}
+	}
+
+	private void rotateDialer(float degrees) {
+		matrix.postRotate(degrees);
+
+		dialer.setImageMatrix(matrix);
 	}
 }
